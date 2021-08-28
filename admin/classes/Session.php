a40ffa6fd4e2277670081715ed65639ad7de9d1f
Jocelyn <duong.huan2122000@gmail.com>
2021-08-28 17:04:38 +0700

    thêm thư viện xử lý session

---
+++ admin/classes/Session.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Lớp session
+ * @author CreatedBy: dbhuan (28/08/2021)
+ */
+class Session
+{
+    /**
+     * Hàm bắt đầu session
+     * @author CreatedBy: dbhuan (28/08/2021)
+     */
+    public function start()
+    {
+        session_start();
+    }
+
+    /**
+     * Hàm lưu session
+     * @author CreatedBy: dbhuan (28/08/2021)
+     */
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Hàm lấy dữ liệu session
+     * @author CreatedBy: dbhuan (28/08/2021)
+     */
+    public function get($key)
+    {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+    }
+
+    /**
+     * Hàm xóa session
+     * @author CreatedBy: dbhuan (28/08/2021)
+     */
+    public function destroy()
+    {
+        session_destroy();
+    }
+}
